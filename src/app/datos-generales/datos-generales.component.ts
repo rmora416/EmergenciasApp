9e911ec60b5e9cbebc4bb967801aa3ee7d114e6c
ssergio <ssergio@mivah.cr>
2022-11-28 09:28:48 -0600

    Agregando Campos a boleta

--- src/app/datos-generales/datos-generales.component.ts
+++ src/app/datos-generales/datos-generales.component.ts
@@ -1,10 +1,10 @@
 
 import { Component, Output, EventEmitter, OnInit, AfterContentInit, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { FormGroup, FormBuilder, Validators  } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Generales_item } from '../Clases/DatosGenerales.class';
 import { catalogo } from '../Clases/Catalogo.class';
-import {BoletaService} from '../Servicios/boleta.service'
-import {CatalogosService} from '../Servicios/catalogos.service'
+import { BoletaService } from '../Servicios/boleta.service'
+import { CatalogosService } from '../Servicios/catalogos.service'
 
 @Component({
   selector: 'app-datos-generales',
@@ -13,36 +13,39 @@ import {CatalogosService} from '../Servicios/catalogos.service'
 })
 export class DatosGeneralesComponent implements OnChanges, OnInit {
 
-  @Input() datos! : Generales_item;
+  @Input() datos!: Generales_item;
   @Output() formReady = new EventEmitter<FormGroup>()
   generalesForm!: FormGroup;
   emergencias: catalogo[] = [];
   tiposAcceso: catalogo[] = [];
   fallasSuministroElectrico: catalogo[] = [];
   instituciones: catalogo[] = [];
+  mostrarInstituciones: boolean = false;
+  campoOtros: boolean = false;
 
   constructor(private fb: FormBuilder, public boletaService: BoletaService, public catalogosService: CatalogosService) { }
 
   ngOnInit(): void {
     this.generalesForm = this.fb.group({
-      ID_Datos_Inspeccion: [''], 
-      EmergenciaID: ['', [Validators.required]], 
+      ID_Datos_Inspeccion: [''],
+      EmergenciaID: ['', [Validators.required]],
       Fecha_Visita: ['', [Validators.required]],
-      Solo_Externa: null, 
-      Valoracion_Realizada: null, 
-      ID_Tipo_Acceso: [''],  
+      Solo_Externa: null,
+      Valoracion_Realizada: null,
+      ID_Tipo_Acceso: [''],
       ID_Fallas_Suministro_Electrico: [''],
-      Focos_Contaminacion: [''], 
-      Informe_Condiciones_Riesgo: [''], 
-      ID_Institucion_Informe: ['', [Validators.required]], 
+      Focos_Contaminacion: [''],
+      Informe_Condiciones_Riesgo: [''],
+      ID_Institucion_Informe: ['', [Validators.required]],
       Otro_Institucion_Informe: ['']
-
     });
+
+
     // Emit the form group to the parent
     this.formReady.emit(this.generalesForm);
 
     this.catalogosService.getEmergencias().subscribe(items => {
-      this.emergencias = items;       
+      this.emergencias = items;
     });
 
     this.catalogosService.getTipo_Acceso().subscribe(items => {
@@ -56,26 +59,40 @@ export class DatosGeneralesComponent implements OnChanges, OnInit {
     this.catalogosService.getInstituciones().subscribe(items => {
       this.instituciones = items;
     });
+
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    if (changes['user'] && !changes['user'].firstChange) { 
-      this.generalesForm.patchValue({        
+  mostarCampoInstitucion(){
+    console.log(this.mostrarInstituciones);
+    if(this.mostrarInstituciones) {
+      this.mostrarInstituciones = false;
+    } else {
+      this.mostrarInstituciones = true;
+    }
+  }
 
-        ID_Datos_Inspeccion: this.datos.ID_Datos_Inspeccion, 
+  
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['user'] && !changes['user'].firstChange) {
+      this.generalesForm.patchValue({
+
+        ID_Datos_Inspeccion: this.datos.ID_Datos_Inspeccion,
         EmergenciaID: this.datos.EmergenciaID,
         Fecha_Visita: this.datos.Fecha_Visita,
-        Solo_Externa: this.datos.Solo_Externa, 
-        Valoracion_Realizada: this.datos.Valoracion_Realizada, 
-        ID_Tipo_Acceso: this.datos.ID_Tipo_Acceso, 
+        Solo_Externa: this.datos.Solo_Externa,
+        Valoracion_Realizada: this.datos.Valoracion_Realizada,
+        ID_Tipo_Acceso: this.datos.ID_Tipo_Acceso,
         ID_Fallas_Suministro_Electrico: this.datos.ID_Fallas_Suministro_Electrico,
         Focos_Contaminacion: this.datos.Focos_Contaminacion,
         Informe_Condiciones_Riesgo: this.datos.Informe_Condiciones_Riesgo,
         ID_Institucion_Informe: this.datos.ID_Institucion_Informe,
         Otro_Institucion_Informe: this.datos.Otro_Institucion_Informe,
 
-      }) 
+      })
     }
   }
 
 }
+
+
